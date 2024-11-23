506c9eaa9353381143000e8396d7a965c8246d2c
jtamu <gtamu.dip@gmail.com>
2024-11-24 00:53:01 +0900

    feat: モジュール化

---
+++ src/Geometry/Cuboid.hs
@@ -0,0 +1,11 @@
+module Geometry.Cuboid
+(
+    volume,
+    area
+) where
+
+volume :: Float -> Float -> Float -> Float
+volume a b c = a * b * c
+
+area :: Float -> Float -> Float -> Float
+area a b c = 2 * (a*b + b*c + c*a)
